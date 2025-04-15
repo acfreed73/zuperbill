42a7ff82db08bb05d5d7ba91a1f3d77e546f7c24
Adam Freed <admin@callitweb.com>
2025-04-15 10:09:54 -0500

    notes(change password): use postgres to change hashed_password

--- backend/start_prod.sh
+++ backend/start_prod.sh
@@ -20,7 +20,13 @@ exec uvicorn app.main:app \
 
 # NOTE:  First Time run we need to create the DB with alembic
 # docker compose -f docker-compose.prod.yml exec backend bash
-# 
+
 # rm alembic/versions/*.py
 # alembic revision --autogenerate -m "clean schema"
 # alembic upgrade head
+
+# export PSQL_URL=$(echo "$DATABASE_URL" | sed 's/+psycopg2//')
+# psql "$PSQL_URL"
+
+# CREATE EXTENSION IF NOT EXISTS pgcrypto;
+# UPDATE users SET hashed_password = crypt('password', gen_salt('bf')) WHERE user_name = 'Adam';
